47bf42a250fcb2d4d55dfb99599642f0ee1462e8
Sazzad Hissain Khan <hissain.khan@gmail.com>
2025-12-07 01:57:17 +0600

    Updated javadoc

--- src/main/java/com/hissain/jscipy/signal/WelchResult.java
+++ src/main/java/com/hissain/jscipy/signal/WelchResult.java
@@ -1,12 +1,23 @@
 package com.hissain.jscipy.signal;
 
 /**
- * Result of Welch's method.
+ * Holds the result of Welch's method for spectral density estimation.
  */
 public class WelchResult {
+    /**
+     * Array of sample frequencies.
+     */
     public final double[] f;
+    /**
+     * Power Spectral Density or power spectrum of x.
+     */
     public final double[] Pxx;
     
+    /**
+     * Constructs a WelchResult.
+     * @param f Array of sample frequencies.
+     * @param Pxx Power Spectral Density or power spectrum.
+     */
     public WelchResult(double[] f, double[] Pxx) {
         this.f = f;
         this.Pxx = Pxx;
